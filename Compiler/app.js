bad7a9d5e3afa52e2b0e094d82cab03dadec29b0
Pouria Afshari <74050766+pouria-007@users.noreply.github.com>
2021-12-16 12:08:11 +0100

    Add files via upload

--- Compiler/app.js
+++ Compiler/app.js
@@ -1,4 +1,18 @@
-var xhr = new XMLHttpRequest();
-xhr.open('POST', 'https://ide.geeksforgeeks.org/main.php');
-xhr.setRequestHeader('authority', 'ide.geeksforgeeks.org');
-xhr.send();
+document.getElementById("run").addEventListener("click", () => {
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', 'https://pooriya007.pythonanywhere.com/');
+    var Mycode = document.getElementById("input").value;
+    xhr.setRequestHeader('code', Mycode);
+    xhr.setRequestHeader('lang', 'py');
+    xhr.send();
+
+    xhr.responseType = 'text';
+    xhr.onload = function () {
+        if (xhr.readyState === xhr.DONE) {
+            if (xhr.status === 200) {
+                //document.getElementById("out").innerText = xhr.response;
+                document.getElementById("out").innerText = xhr.responseText;
+            }
+        }
+    };
+})
